84681c28baaee5b80644aece6f4a39de34743c1a
PaulJame5 <c00231934@itcarlow.ie>
2020-09-30 00:28:09 +0100

    Implemented Factorial calculation of a number

---
+++ Assets/Scripts/FactorialFilter.cs
@@ -0,0 +1,35 @@
+﻿
+public class FactorialFilter
+{
+   public static int Factorial(int number)
+    {
+        if(number < 0)
+        {
+            return -1;
+        }
+
+        if(number == 0)
+        {
+            return 1;
+        }
+
+        int result = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static int[] Factorials(int[] numbers)
+    {
+        int[] result = new int[numbers.Length];
+
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            result[i] = Factorial(numbers[i]);
+        }
+        return result;
+    }
+}
