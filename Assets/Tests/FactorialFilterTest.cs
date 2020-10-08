cd73604c5c4c26c50882e1859d0f5ffa7580cf14
PaulJame5 <c00231934@itcarlow.ie>
2020-10-08 23:27:58 +0100

    Updated Implementation Tests

--- Assets/Tests/FactorialFilterTest.cs
+++ Assets/Tests/FactorialFilterTest.cs
@@ -5,7 +5,7 @@ namespace Tests
     public class FactorialFilterTest
     {
         [Test]
-        public void FactorialFilterTestSimple()
+        public void FactorialFilterTestInt()
         {
             int[] input = { 4, 2, 8, 3, 9, 4, 10 };
             int[] output = FactorialFilter.Factorials(input);
@@ -14,11 +14,52 @@ namespace Tests
             CollectionAssert.AreEqual(expected, output);
         }
 
+        [Test]
         public void FactorialFilterTestLong()
         {
             long[] input = { 4, 2, 8, 3, 9, 4, 10 };
             long[] output = FactorialFilter.FactorialsLong(input);
             long[] expected = { 24, 2, 40320, 6, 362880, 24, 3628800 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void FactorialFilterZeroTest()
+        {
+            int[] input = { 0 };
+            int[] output = FactorialFilter.Factorials(input);
+            int[] expected = { 1 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void FactorialFilterZeroTestLong()
+        {
+            long[] input = { 0 };
+            long[] output = FactorialFilter.FactorialsLong(input);
+            long[] expected = { 1 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void FactorialFilterNegativeTest()
+        {
+            int[] input = { -100, -90909, -111001, -1 };
+            int[] output = FactorialFilter.Factorials(input);
+            int[] expected = { -1, -1, -1, -1};
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void FactorialFilterNegativeTestLong()
+        {
+            long[] input = { -100, -90909, -111001, -1 };
+            long[] output = FactorialFilter.FactorialsLong(input);
+            long[] expected = { -1, -1, -1, -1 };
 
             CollectionAssert.AreEqual(expected, output);
         }
